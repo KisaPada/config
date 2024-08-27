4186e54e8e709196e425889b8eb3a1019e7ca839
KisaPada <jovan-3@hotmail.com>
2024-08-27 15:08:40 -0400

    added /neovim/init.lua

---
+++ neovim/init.lua
@@ -0,0 +1,6 @@
+vim.o.tabstop = 4           -- Number of spaces that a <Tab> in the file counts for
+vim.o.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
+vim.o.expandtab = true      -- Use spaces instead of tabs
+
+vim.o.number = true         -- Enable line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
