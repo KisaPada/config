b4eba8f052d082d96ac6a8540bf3b9e72518c1d5
KisaPada <jovan-3@hotmail.com>
2024-08-30 12:44:41 -0400

    updated init.lua to use vim.opt instead of vim.o

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,9 +1,10 @@
-vim.o.tabstop = 4           -- Number of spaces that a <Tab> in the file counts for
-vim.o.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
-vim.o.expandtab = true      -- Use spaces instead of tabs
+vim.opt.tabstop = 4           -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true      -- Use spaces instead of tabs
 
-vim.o.number = true         -- Enable line numbers
-vim.o.relativenumber = true -- Enable relative line numbers
+vim.opt.number = true         -- Enable line numbers
+vim.opt.relativenumber = true -- Enable relative line numbers
 
-vim.o.linebreak = true      -- Soft-wrap lines at word boundaries
-vim.o.breakindent = true    -- Enable indentation on soft-wrapped lines
+vim.opt.linebreak = true      -- Soft-wrap lines at word boundaries
+vim.opt.breakindent = true    -- Enable indentation on soft-wrapped lines
+
